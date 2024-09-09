e7949f41d20f635f15ae34ad87d048e8299362f0
akomathu <pc-504@st-ove.co.jp>
2024-09-09 15:19:29 +0900

    問題と答えが交互に切り替わるようにした

--- frontend/my-app/src/component/word-cards-display/Cards.tsx
+++ frontend/my-app/src/component/word-cards-display/Cards.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -6,9 +6,16 @@ import Typography from '@mui/material/Typography';
 
 
 function Cards() {
+const [clickCardLength, setClickCardLength] = useState(0)
+
+  const handleClickCard = () =>{
+  setClickCardLength(clickCardLength + 1)
+  console.log(clickCardLength)
+}
+  
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Card sx={{ minWidth: 275, width: 400, height: 200  }}>
+      <Card sx={{ minWidth: 275, width: 400, height: 200  }} onClick={handleClickCard}>
       <CardContent sx={{
             display: 'flex',
             justifyContent: 'center',   
@@ -16,13 +23,17 @@ function Cards() {
             height: '100%',   
             textAlign: 'left',          
           }}>
+            
       <Typography gutterBottom sx={{ 
             color: 'text.secondary', 
             fontSize: 22, 
             wordWrap: 'break-word',
             whiteSpace: 'normal', 
             maxWidth: '100%', }}>
-              単語・word・タンゴ
+           {clickCardLength %2 === 0
+           ? "問題（単語）"
+           : "答え"
+           }
         </Typography>
       </CardContent>
       </Card>
